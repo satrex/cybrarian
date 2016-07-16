778effdaef2da166bbe8f1b2aea57560487d028e
satrex <info@satrex.jp>
2016-07-16 18:48:35 +0900

    added index.js to try auto deploying

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
+
+var app = express();
+var users;
+
+app.use(express.static('build'));
+app.use(bodyParser.json());
+app.listen(3000);
